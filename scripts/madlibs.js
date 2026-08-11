3cdc0067202f78a951fe7612ac1b8588c42c9008
Cindy <cindino45@gmail.com>
2026-08-10 21:30:33 -0500

    add new card

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,13 +1,15 @@
 let name=prompt("A name:");
 let adjective=prompt("An adjective:");
 let noun=prompt("A noun:");
-
+let food=prompt("A food:");
+let game=prompt("A game:");
 
 
 function makeCard(){
 	
 	console.log("Dear " + name + ",");
-	console.log("You are very " + adjective + " , may people bring you plenty of  " + noun + "s on your Birthday!");
+	console.log("You are very " + adjective + " , may people bring you plenty of " + noun + "s on your Birthday!");
+	console.log("If you eat more " + food + " and play  more " + game + ", you'll live longer!");
 	console.log("Happy Birthday from the generator.");
 }
 
