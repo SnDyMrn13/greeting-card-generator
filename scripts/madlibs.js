db433a0a07c610ea54ca94924a288f57fd2e25e9
Cindy <cindino45@gmail.com>
2026-08-10 20:56:53 -0500

    name the card block

---
+++ scripts/madlibs.js
@@ -0,0 +1,18 @@
+let name=prompt("A name:");
+let adjective=prompt("An adjective:");
+let noun=prompt("A noun:");
+
+
+
+function makeCard(){
+	
+	console.log("Dear " + name + ",");
+	console.log("You are very " + adjective + " , may people bring you plenty of  " + noun + "s on your Birthday!");
+	console.log("Happy Birthday from the generator.");
+}
+
+
+
+makeCard();
+
+
