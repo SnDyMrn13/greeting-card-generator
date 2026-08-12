1cab743c9f560ecd8f0cf09c5487489651b2440e
Cindy <cindino45@gmail.com>
2026-08-11 20:35:33 -0500

    speed and stop experiments

--- scripts/tick.js
+++ scripts/tick.js
@@ -1,10 +1,14 @@
 let count=0;
-
+let speed=1000;
 
 function tick(){
 	count=count + 1;
 	console.log("Tick " + count);
+	if(count===10){
+		clearInterval(timer);
+		console.log("The counter rests.");
+	}
 }
 
 
-setInterval(tick, 1000);
+let timer=setInterval(tick, speed);
