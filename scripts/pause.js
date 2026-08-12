227abb4124daae3f46d1c25f9c9624838035312b
Cindy <cindino45@gmail.com>
2026-08-11 20:14:53 -0500

    copy of scripts/madlibs file

--- scripts/pause.js
+++ scripts/pause.js
@@ -3,5 +3,5 @@ function makeCard(){
 	console.log(" you are as brave as a volcano.");
 }
 console.log("Warming up the generator...");
-setTimeout(makeCard, 5000);
+setTimeout(makeCard, 3000);
 console.log("Generator loaded.  Card in 3 seconds.");
