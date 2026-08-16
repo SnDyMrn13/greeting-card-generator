460e9539a658e1907b9fd3b2fd5fdede616d6641
Cindy <cindino45@gmail.com>
2026-08-15 21:39:43 -0500

    make every card end with random closing, closes #3

--- card.js
+++ card.js
@@ -3,9 +3,30 @@ let adjectiveInput=document.getElementById("adjective-input");
 let nounInput=document.getElementById("noun-input");
 let foodInput=document.getElementById("food-input");
 let gameInput=document.getElementById("game-input");
-
 let makeButton=document.getElementById("make-button");
 let cardText=document.getElementById("card-text");
+let index=document.getElementById("closing-text");
+
+let closings=[
+	"You are Legend!",
+	"Stay Golden.",
+	"Signs point to Awesome!",
+	"You got this!",
+	"You are Amazing!",
+	"Greatness is within you.",
+	"You can do it!",
+	"You are Grrreat!",
+	
+	]
+	
+function randomNumber(a,b){
+	return Math.floor(Math.random() * (b - a + 1)) + a;
+		
+}
+	
+
+
+	
 
 function makeCard(){
 	
@@ -15,16 +36,21 @@ function makeCard(){
 	let food=foodInput.value;
 	let game=gameInput.value;
 	
+	let index=closings[randomNumber(0,closings.length - 1)];
 	
-
+	cardText.textContent=
+		"Dear " + friendName + 
+		".  You are very " + adjective + ",  may people bring you plenty of " + noun + "s on your Birthday! " + 
+		"  If you eat more " + food + " and play more " + game + ", you'll live longer!  " + index ;
+		
 		
 	
 	
 	
-	cardText.textContent=
-		"Dear " + friendName + 
-		", You are very " + adjective + " , may people bring you plenty of " + noun + "s on your Birthday!" + 
-		"  If you eat more " + food + " and play more " + game + ", you'll live longer!";
+	
 }
 
+	
+	
 makeButton.addEventListener("click", makeCard);
+
